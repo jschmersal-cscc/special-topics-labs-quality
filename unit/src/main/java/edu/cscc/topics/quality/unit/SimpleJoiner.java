a93045d99b41c7c20ca41fad43e89152d1d47970
melanie-oneill <moneill6@student.cscc.edu>
2019-11-19 20:48:07 -0500

    finished TODOs

--- unit/src/main/java/edu/cscc/topics/quality/unit/SimpleJoiner.java
+++ unit/src/main/java/edu/cscc/topics/quality/unit/SimpleJoiner.java
@@ -5,8 +5,10 @@ public class SimpleJoiner implements Joiner{
     public String join(String separator, String base, String... parts) {
         String accumulation = base;
         for (int i = 0; i < parts.length; i++) {
-            accumulation = accumulation + separator + parts[i];
+            if (parts[i] != null) {
+                accumulation = accumulation + separator + parts[i];
+            }
+        } return accumulation;
         }
-        return accumulation;
     }
-}
+
